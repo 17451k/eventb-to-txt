fb97790ab18b8ebd69790c5f7510aa940aab409d
Ilya Shchepetkov <ilya.shchepetkov@gmail.com>
2023-06-20 14:33:51 +0400

    Update list of supported Python versions

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description="Event-B to txt converter",
     long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     url="https://github.com/17451k/eventb-to-txt",
     packages=['eventb_to_txt'],
     entry_points={
@@ -23,11 +23,11 @@ setuptools.setup(
     },
     classifiers=(
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
